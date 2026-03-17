746d68caf4b23d88afffcd484cee7fa8594dcbff
Huseyin Tozluyurt <huseyin_tozluyurt@hotmail.com>
2026-03-17 09:32:20 +0300

    Add files via upload

---
+++ webcam_test.py
@@ -0,0 +1,16 @@
+import cv2
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
+        break
+    cv2.imshow('Webcam', frame)
+    cv2.waitKey(20)
+    
+    #if cv2.waitKey(1) == ord('q'):
+    #    break
+cap.release()
+cv2.destroyAllWindows() 
+
+
